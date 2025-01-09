1328106cb20a4852c5745bc07312068cb4202786
Ethan Chen <echen@lcs.on.ca>
2025-01-09 14:54:37 -0500

    Initial Commit

---
+++ LottieAnimationsList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LottieAnimationsList
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-09.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
